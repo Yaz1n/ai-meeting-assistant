ee276dcacb37f94cc58a70abb7193aa622f7ad04
Yaz1n <muhammedyasinas@gmail.com>
2025-06-30 09:51:39 +0530

    initialise frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">AI Meeting Assistant</h1>
+    </div>
+  );
+}
+
+export default App;
